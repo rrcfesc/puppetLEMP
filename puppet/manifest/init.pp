84c3a7d1b0162c08856a55924b5b222737a18f69
Ricardo Jesus Ruiz Cruz <kasparov@Mac-mini-de-Ricardo.local>
2017-01-08 21:23:44 -0600

    Integrando cambios de nginx y de configuración de php

--- puppet/manifest/init.pp
+++ puppet/manifest/init.pp
@@ -5,11 +5,160 @@ if versioncmp($::puppetversion,'3.6.1') >= 0 {
     }
 }
 #########
-node web {
+node web01 {
+    include pckgsextra
+    include proyecto
+}
+node /^app0[0-9]/ {
     include pckgsextra
 }
 class pckgsextra{
     package { ['git','ant','openvpn' , 'tar', 'telnet', 'java-1.7.0-openjdk-devel', 'java-1.7.0-openjdk', 'nano'] :
         ensure  => present,
+    }
+    file { '/etc/hosts':
+        ensure  => file,
+        owner => root,
+        group => root,
+        mode  => 644,
+        content => "
+        192.168.100.14 base.local.com
+        192.168.100.13 cache.local.com
+        ",
+    }
+    file { '/etc/environment':
+        ensure  => file,
+        owner => root,
+        group => root,
+        mode  => 644,
+        content => "
+        LANG=es_MX.utf-8
+        LC_ALL=es_MX.utf-8",
+    }
+    
+}
+class networking {
+
+}
+class websrv {
+    class { 'nginx': sendfile => off } 
+
+    user { "nginx":
+        ensure     => present,
+        gid        => "nginx",
+        groups     => ["vagrant","apache"],
+         # For the user to exist
+        require => [Group['nginx'],Group['vagrant']]
+    }
+    user { "vagrant":
+        ensure     => present,
+        gid        => "vagrant",
+        groups     => ["vagrant","nginx","apache"],
+        require => [Group['nginx'],Group['vagrant'],Group['apache']]
+    }
+    group {"nginx":
+        ensure     => present,
+    }
+    group {"vagrant":
+        ensure     => present,
+    }
+    group {"apache":
+        ensure     => present,
+    }
+}
+#############================#############
+class appsrv {
+    #require yum::repo::remi
+    require yum::repo::epel
+    require yum::repo::remi_php56
+    # For the user to exist
+    require websrv
+    package { 'libtidy':
+        ensure  => present,
+    }
+    package { 'libtidy-devel':
+        ensure  => present,
+    }
+    package { 'php-tidy':
+        ensure  => present,
+    }
+    class { php::fpm::daemon:
+        log_owner => 'nginx',
+        log_group => 'nginx',
+        log_dir_mode => '0775',
+    }
+    php::fpm::conf { 'www':
+        listen  => '127.0.0.1:9001',
+        user    => 'nginx',
+    }
+    php::module { [ 'pecl-apcu',
+        'pear',
+        'pdo',
+        'mysqlnd',
+        'pgsql',
+        'pecl-mongo',
+        'pecl-sqlite',
+        'mbstring',
+        'mcrypt',
+        'xml',
+        'php-devel',
+        'pecl-memcached',
+        'gd',
+        'soap']:
+        notify  => Service['php-fpm'],
+    }
+    php::ini { '/etc/php.ini':
+        short_open_tag              => 'On',
+        asp_tags                    => 'Off',
+        date_timezone               => 'America/Mexico_City',
+        error_reporting             => 'E_ALL & ~E_DEPRECATED',
+        display_errors              => 'On',
+        html_errors                 => 'On',
+        notify  => Service['php-fpm'],
+    }
+    file { '/var/log/php-fpm/www-error.log':
+        ensure => "file",
+        owner  => "nginx",
+        group  => "nginx",
+        mode   => 777
+    }
+    file { '/var/log/php-fpm/error.log':
+        ensure => "file",
+        owner  => "nginx",
+        group  => "nginx",
+        mode   => 777,
+    }
+}
+#############================#############
+class proyecto {
+    require websrv    
+    nginx::resource::vhost { 'www.proyecto.local.com':
+        www_root => '/www/www.proyecto.local.com/public',
+        ssl => true,
+        #location        => '/',
+        ssl_cert             => '/vagrant/puppet/certs/server.crt',
+        ssl_key              => '/vagrant/puppet/certs/server.key',
+        index_files           => [ 'index.php' ],
+        use_default_location => true,
+        location_cfg_append => { 
+            try_files => '$uri $uri/ /index.php$is_args$args'
+        }
+    }
+    nginx::resource::location { "www.proyecto.local.com":
+        ensure          => present,
+        ssl              => true,
+        vhost           => 'www.proyecto.local.com',
+        www_root        => '/www/www.proyecto.local.com/public',
+        location        => '~ \.php$',
+        index_files     => ['index.php'],
+        proxy           => undef,
+        fastcgi         => "127.0.0.1:9001",
+        fastcgi_script  => undef,
+        location_cfg_append => { 
+            fastcgi_connect_timeout => '5h',
+            fastcgi_read_timeout    => '5h',
+            fastcgi_send_timeout    => '5h',
+            fastcgi_param    => "APPLICATION_ENV 'development'"
+        }
     }
 }
